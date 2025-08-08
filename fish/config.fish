eb5fcd9b742ebd5efccd576cb90dc4164aec0d81
Nikhil Singh <nikhilgangrial@gmail.com>
2025-08-09 02:50:56 +0530

    Starship: Add starship prompt

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
 	zoxide init fish | source
-    # Commands to run in interactive sessions can go here
+  starship init fish | source
+  # Commands to run in interactive sessions can go here
 end
 
 mise activate fish | source # added by https://mise.run/fish
